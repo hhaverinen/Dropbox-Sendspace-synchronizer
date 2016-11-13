6582731032a8f9fb2a6daf6229580a9ce9884d41
Henri Haverinen <henkka.haverinen@gmail.com>
2016-11-13 12:23:09 +0200

    get api token from sendspace

--- synchronizer.js
+++ synchronizer.js
@@ -1,8 +1,11 @@
 var Dropbox = require('dropbox');
+var request = require('request');
+var xml2js = require('xml2js');
 
 var db_key = '3a30dgfp45ufpmh';
 var db_secret = '2hy2qujs4vdnxr4';
 var db_accesstoken = '8bL7cF9QDZAAAAAAAAAACchf4Ih1b_lcB_dVSnSUpcsZLmao1IU-FCXSjbK8bvi0'; //TODO: get this with OAUTH
+var ss_key = 'LN5IKXSIF8';
 
 var dbx = new Dropbox({ accessToken: db_accesstoken });
 
@@ -36,8 +39,21 @@ var listFiles = function(dbx, cursor) {
     }
 }
 
+var getSendspaceSessionkey = function() {
+    var token;
+    request('http://api.sendspace.com/rest/?method=auth.createtoken&api_key=LN5IKXSIF8&api_version=1.2', function(error, response, body) {
+        var parser = new xml2js.Parser();
+        
+        parser.parseString(body, function(err, result) {
+            token = result.result.token[0];
+            console.log(token);
+        });
+    });
+}
+
 module.exports = function() {
-    listFiles(dbx);
+    //listFiles(dbx);
+    getSendspaceSessionkey();
     
     return "Terve mualima!";
 }
