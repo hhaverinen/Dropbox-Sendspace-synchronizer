c8cbbb6a53496158c14e05919a2d77758466cd9f
Henri Haverinen <henkka.haverinen@gmail.com>
2016-11-15 20:25:11 +0200

    uploading a local test file to sendspace server via API

--- synchronizer.js
+++ synchronizer.js
@@ -2,6 +2,7 @@ var Dropbox = require('dropbox');
 var request = require('request');
 var xml2js = require('xml2js');
 var md5 = require('md5');
+var fs = require('fs');
 
 var db_key = '3a30dgfp45ufpmh';
 var db_secret = '2hy2qujs4vdnxr4';
@@ -40,7 +41,8 @@ var listFiles = function(dbx, cursor) {
     }
 }
 
-var getSendspaceSessionkey = function() {
+// gets sendspace sessionkey for current session
+var getSendspaceSessionkey = function(handler) {
     var token;
     request('http://api.sendspace.com/rest/?method=auth.createtoken&api_key=LN5IKXSIF8&api_version=1.2', function(error, response, body) {
         var parser = new xml2js.Parser({explicitArray: false});
@@ -50,21 +52,42 @@ var getSendspaceSessionkey = function() {
         
         if (token) {
             var email = 'henrin.testailu@gmail.com';
-            var pw = 'passwordhere';
+            var pw = 'passwordHere';
             var tokened_pw = md5(token + md5(pw).toLowerCase()).toLowerCase();
             
             request('http://api.sendspace.com/rest/?method=auth.login&token='+token+'&user_name='+email+'&tokened_password='+tokened_pw, function(error, response, body) {
                 parser.parseString(body, function(err, result) {
-                    return result.result.session_key;
+                    handler(result.result.session_key);
                 });
             });
         }
     });
 }
 
+var getUploadUrl = function(session_key) {
+    request('http://api.sendspace.com/rest/?method=upload.getinfo&session_key='+session_key, function(error, response, body) {
+        var parser = new xml2js.Parser({explicitArray: false});
+        parser.parseString(body, function(err, result) {
+            var uploadObj = result.result.upload.$;
+            console.log(uploadObj);
+            
+            var formData = {
+                MAX_FILE_SIZE: uploadObj.max_file_size,
+                UPLOAD_IDENTIFIER: uploadObj.upload_identifier,
+                extra_info: uploadObj.extra_info,
+                userfile: fs.createReadStream('testi.txt')
+            };
+
+            request.post({url:uploadObj.url, formData: formData}, function(error, response, body) {
+                console.log(body);
+            });
+        });
+    });
+}
+
 module.exports = function() {
     //listFiles(dbx);
-    console.log(getSendspaceSessionkey()); // returns undefined because requests are made asynchronically
+    getSendspaceSessionkey(getUploadUrl);
     
     return "Terve mualima!";
 }
