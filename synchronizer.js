0f7a3f31e85cfd4429f6050e24e6e5f48fa97f6c
Henri Haverinen <henkka.haverinen@gmail.com>
2016-11-27 18:44:26 +0200

    Refactoring and some commenting

--- synchronizer.js
+++ synchronizer.js
@@ -1,68 +1,28 @@
 var Dropbox = require('dropbox');
 var SendSpace = require('./SendSpaceClient/SendSpace');
-var request = require('request');
+var dropboxUtils = require('./utils/dropbox-utils');
 
 var dbConf = require('./config/dropboxConfig');
 var ssConf = require('./config/sendspaceConfig');
-
-// gets all the files from dropbox recursively
-var getAllDropBoxFilesAndFolders = function(dbx, cursor, contents) {
-    return new Promise(function(resolve, reject) {
-        var contents = contents || [];
-
-        var responseHandler = function(response) {
-            contents = contents.concat(response.entries);
-            console.log(response.has_more);
-
-            if (response.has_more) {
-                getAllDropBoxFilesAndFolders(dbx, response.cursor, contents).then(function(contents) {
-                    resolve(contents);
-                }).catch(function(error) {
-                    reject(error);
-                });
-            } else {
-                resolve(contents);
-            }
-        }
-
-        if(!cursor) {
-            console.log("call files list folder");
-            dbx.filesListFolder({ path: '', recursive: true }).then(responseHandler).catch(function(error) {
-                reject(error);
-            });
-        } else {
-            console.log("call files list folder continue");
-            dbx.filesListFolderContinue({ cursor: cursor }).then(responseHandler).catch(function(error) {
-                reject(error);
-            });
-        }
-    });
-}
-
-var getDropBoxDownloadParams = function(filepath, authToken) {
-    return {
-        url: 'https://content.dropboxapi.com/2/files/download',
-        headers: {
-            'Dropbox-API-Arg': JSON.stringify({path: filepath}),
-            'Authorization': 'Bearer '+authToken
-        }
-    }
-}
 
 module.exports = function() {
     var dbx = new Dropbox(dbConf);
     var ss = new SendSpace(ssConf);
 
-    ss.startSession().then(function(response) {
+    // starts new sendspace session
+    ss.startSession().then(function() {
 	    console.log('=== Login to sendspace successful!');
 
-        return ss.getAllFoldersAndFiles().then(function(response) {
+        // populates sendspace object with information of all existing files and folders
+        return ss.getAllFoldersAndFiles().then(function() {
             console.log('=== Got all existing files and folders from sendspace!');
 
-            return getAllDropBoxFilesAndFolders(dbx).then(function(entries) {
+            // gets array of the files and folders from the dropbox
+            return dropboxUtils.getAllDropBoxFilesAndFolders(dbx).then(function(entries) {
                 console.log('=== Got all files and folders from dropbox');
                 var folders = [], files = [];
 
+                // seperate folders and files to own arrays
                 entries.forEach(function(item) {
                     if (item['.tag'] == 'folder') {
                         folders.push(item.path_display); // only paths are needed
@@ -71,21 +31,24 @@ module.exports = function() {
                     }
                 });
 
-                //sync folders
-                return ss.syncFolders(folders).then(function(response) {
+                // synchronize folders from dropbox to sendspace
+                return ss.syncFolders(folders).then(function() {
                     console.log('=== Synced all folders from dropbox to sendspace!');
 
-                    //sync files
+                    // synchronize files from dropbox to sendspace
                     return Promise.all(files.map(function(item) {
+                        // synchronize file only if it doesn't exist in sendspace yet
                         if(!ss.fileExists(item.path_display)){
                             console.log("=== Started syncing file from dropbox to sendspace: " + JSON.stringify(item));
+                            // just for making reading params easier
                             var name = item.name;
                             var path = item.path_display;
                             var folder = ss.getParentFolder(path);
-                            var dropboxDownloadParams = getDropBoxDownloadParams(path, dbConf.accessToken);
-                            return ss.uploadFileToSendSpace(name, request.get(dropboxDownloadParams), folder);
+                            var dropBoxDownloadStream = dropboxUtils.getDropBoxDownloadParams(path, dbConf.accessToken);
+                            // do the actual uploading of the file to sendspace
+                            return ss.uploadFileToSendSpace(name, dropBoxDownloadStream, folder);
                         }
-                    })).then(function(response) {
+                    })).then(function() {
                         console.log('=== Synced all files from dropbox to sendspace!');
                     });
 
@@ -97,6 +60,7 @@ module.exports = function() {
 	    console.log('=== Error happened!');
         console.log(error);
     }).then(function() {
+        // end sendspace session
         ss.endSession().then(function() {
             console.log('=== Successfully logout from sendspace!');
         }).catch(function(error) {
@@ -105,5 +69,6 @@ module.exports = function() {
         });
     });
 
+    // TODO: give corresponding response to the client how the synchronizing went
     return "Terve mualima!";
 }
---
+++ utils/dropbox-utils.js
@@ -0,0 +1,71 @@
+var request = require('request');
+
+var utils = {};
+
+/**
+ * Gets all the files and folders from the dropbox recursively
+ * @param dbx Dropbox object. See https://github.com/dropbox/dropbox-sdk-js
+ * @param cursor possible cursor from the dropbox response
+ * @param contents this is recursively filled with contents
+ * @return {Promise<Array.<Object>] Promise to the array containing all files and folders from the dropbox
+ */
+utils.getAllDropBoxFilesAndFolders = function(dbx, cursor, contents) {
+    return new Promise(function(resolve, reject) {
+        var contents = contents || [];
+
+        var responseHandler = function(response) {
+            contents = contents.concat(response.entries);
+            console.log(response.has_more);
+
+            if (response.has_more) {
+                this.getAllDropBoxFilesAndFolders(dbx, response.cursor, contents).then(function(contents) {
+                    resolve(contents);
+                }).catch(function(error) {
+                    reject(error);
+                });
+            } else {
+                resolve(contents);
+            }
+        }
+
+        if(!cursor) {
+            console.log("call files list folder");
+            dbx.filesListFolder({ path: '', recursive: true }).then(responseHandler).catch(function(error) {
+                reject(error);
+            });
+        } else {
+            console.log("call files list folder continue");
+            dbx.filesListFolderContinue({ cursor: cursor }).then(responseHandler).catch(function(error) {
+                reject(error);
+            });
+        }
+    });
+}
+
+/**
+ * Gets params for downloading file from the dropbox
+ * @param filepath path to the downloadable file
+ * @param authToken authentication token
+ * @return {{url: string, headers: {Dropbox-API-Arg, Authorization: string}}}
+ */
+utils.getDropBoxDownloadParams = function(filepath, authToken) {
+    return {
+        url: 'https://content.dropboxapi.com/2/files/download',
+        headers: {
+            'Dropbox-API-Arg': JSON.stringify({path: filepath}),
+            'Authorization': 'Bearer '+authToken
+        }
+    }
+}
+
+/**
+ * Gets stream for downloading file from the dropbox
+ * @param filepath path to the downloadable file
+ * @param authToken authentication token
+ * @return {Request}
+ */
+utils.getDropBoxFileDownloadStream = function(filepath, authToken) {
+    return request.get(this.getDropBoxDownloadParams(filepath, authToken));
+}
+
+module.exports = utils;
