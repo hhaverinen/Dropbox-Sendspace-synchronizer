d836f9f024dbdf62d77ecd7f1a73d93392b30c1f
Henri Haverinen <henkka.haverinen@gmail.com>
2016-12-01 19:17:19 +0200

    javascript validation for form

--- public/views-index.js
+++ public/views-index.js
@@ -1,20 +1,57 @@
+/**
+ * validates form fields
+ * @return {boolean} true if everything's alright, false otherwise
+ */
+var validateFields = function() {
+    var $user = $('#sendspace-form input[name=user');
+    var $password = $('#sendspace-form input[name=password');
+    var $dbaccesstoken = $('#sendspace-form input[name=dbaccesstoken');
+
+    if (!$dbaccesstoken.val()) {
+        $('#response').text('Please authenticate with dropbox.');
+        return false;
+    }
+
+    if (!$user.val() || !$password.val()) {
+        $('#response').text('Please give sendspace credentials.');
+        return false;
+    }
+
+    return true;
+
+}
+
+/**
+ * starts synchronization process
+ * @param e event
+ */
+var startSync = function(e) {
+    e.preventDefault();
+    // only start synchronization if form fields are alright
+    if (validateFields()) {
+
+        // clear error message
+        $('#response').text('');
+
+        var $form = $('#sendspace_form');
+
+        $.ajax({
+            type: 'POST',
+            url: '/synchronize',
+            data: $form.serialize(),
+            success: function (result) {
+                console.log(result);
+                $('#response').text(result);
+            }
+        });
+    }
+}
+
 $(function() {
-    // function for starting the synchrornization progress when '#sync-start' button is clicked
-   $('#sync-start').click(function(e) {
-       e.preventDefault();
-       var $form = $('#sendspace_form');
+    // bind method to button
+   $('#sync-start').click(startSync);
 
-       $.ajax({
-           type: 'POST',
-           url: '/synchronize',
-           data: $form.serialize(),
-           success: function(result) {
-               console.log(result);
-               $('#response').text(result);
-           }
-       });
-   });
-
+    // ajax-spinners
 }).ajaxStart(function() {
     $('#ajax-loader').show();
     $('#blanket').show();
